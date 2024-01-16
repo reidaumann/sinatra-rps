101df79b511777b5ad7f9d4350a2d177368118be
Reid Aumann <141674197+reidaumann@users.noreply.github.com>
2024-01-16 21:14:12 +0000

    defining routes in app.rb

--- app.rb
+++ app.rb
@@ -1,9 +1,47 @@
 require "sinatra"
 require "sinatra/reloader"
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
-end
+moves = ["rock", "paper", "scissors"]
+get("/"){
+  
+  erb(:homepage)
+}
+get("/rock"){
+  @my_move = "rock"
+  @comp_move = moves.sample
+
+  if @comp_move == @my_move
+      @result = "tied"
+  elsif @comp_move == "paper"
+      @result = "lost"
+  else
+      @result = "won"
+  end
+  erb(:rock)
+}
+get("/paper"){
+  @my_move = "paper"
+  @comp_move = moves.sample
+
+  if @comp_move == @my_move
+    @result = "tied"
+  elsif @comp_move == "scissors"
+    @result = "lost"
+  else
+      @result = "won"
+  end
+erb(:paper)
+}
+get("/scissors"){
+  @my_move = "scissors"
+  @comp_move = moves.sample
+
+  if @comp_move == @my_move
+    @result = "tied"
+  elsif @comp_move == "rock"
+      @result = "lost"
+  else
+      @result = "won"
+  end
+  erb(:scissors)
+}
